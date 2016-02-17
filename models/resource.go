74f9b13d05b9633c2b000e80bf5954b796cfbb80
ahmadrezamontazerolghaem <ahmadreza.20012@gmail.com>
2016-02-17 11:02:47 +0330

    Create resource.go

---
+++ models/resource.go
@@ -0,0 +1,37 @@
+type Resource struct {
+	Id   string
+	Name string
+
+	Reason string
+	Status string
+	Type   string
+
+	Time time.Time
+}
+
+type Resources map[string]Resource
+
+func ListResources(app string) (Resources, error) {
+	res, err := CloudFormation().DescribeStackResources(&cloudformation.DescribeStackResourcesInput{
+		StackName: aws.String(app),
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	resources := make(Resources, len(res.StackResources))
+
+	for _, r := range res.StackResources {
+		resources[*r.LogicalResourceId] = Resource{
+			Id:     cs(r.PhysicalResourceId, ""),
+			Name:   cs(r.LogicalResourceId, ""),
+			Reason: cs(r.ResourceStatusReason, ""),
+			Status: cs(r.ResourceStatus, ""),
+			Type:   cs(r.ResourceType, ""),
+			Time:   ct(r.Timestamp),
+		}
+	}
+
+	return resources, nil
+}
