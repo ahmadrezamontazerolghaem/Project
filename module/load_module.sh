30504f895d3f30f99ef9286652b9d39e5e29492a
ahmadrezamontazerolghaem <ahmadreza.20012@gmail.com>
2016-02-17 20:23:57 +0330

    Create load_module.sh

---
+++ module/load_module.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+module="test"
+device="nothing"
+mode="664"
+
+#$* is all the script arguments for the module
+insmod $module.ko $* || exit 1
+
+major=$(awk "\\$2==\"$module\" {print \\$1}" /proc/devices)
+echo major=$major
+mknod /dev/${devices}0 c $major 0
+mknod /dev/${devices}1 c $major 1
+
+group="staff"
+grep -q '^staff:' /etc/group || group="wheel"
+
+chgrp $group /dev/${device}[0-3]
+chmod $mode /dev/${device}[0-3]
