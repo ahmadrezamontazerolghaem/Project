769c510fcf86527136534356bd329345fc151407
ahmadrezamontazerolghaem <ahmadreza.20012@gmail.com>
2016-02-17 11:23:27 +0330

    Create udp-turn-over-tcp.h

---
+++ udp/udp-turn-over-tcp.h
@@ -0,0 +1,17 @@
+#ifndef _UDP_TURN_OVER_TCP_H
+#define _UDP_TURN_OVER_TCP_H
+
+#include "socket.h"
+#include "agent.h"
+
+G_BEGIN_DECLS
+
+
+NiceSocket *
+nice_udp_turn_over_tcp_socket_new (NiceSocket *base_socket,
+    NiceTurnSocketCompatibility compatibility);
+
+
+G_END_DECLS
+
+#endif /* _UDP_TURN_OVER_TCP_H */
