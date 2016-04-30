305482ae2ea89287d1fd67851ac9208fe475754c
ahmadrezamontazerolghaem <ahmadreza.20012@gmail.com>
2016-04-30 15:33:24 +0430

    Update rtmp_bandwidth.c

--- module/rtmp_bandwidth.c
+++ module/rtmp_bandwidth.c
@@ -6,7 +6,7 @@
 void
 ngx_rtmp_update_bandwidth(ngx_rtmp_bandwidth_t *bw, uint32_t bytes)
 {
-    if (ngx_cached_time->sec > bw->intl_end) {
+    if (ngx_cached_time->sec > bw->intl_end) { 
         bw->bandwidth = ngx_cached_time->sec >
             bw->intl_end + NGX_RTMP_BANDWIDTH_INTERVAL
             ? 0
