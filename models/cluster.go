bd1a5fdf8372263b385b889eb09d45da58f1c0a5
ahmadrezamontazerolghaem <ahmadreza.20012@gmail.com>
2016-04-30 15:04:12 +0430

    Update cluster.go

--- models/cluster.go
+++ models/cluster.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/convox/rack/Godeps/_workspace/src/github.com/aws/aws-sdk-go/aws"
 	"github.com/convox/rack/Godeps/_workspace/src/github.com/aws/aws-sdk-go/service/ecs"
-	"github.com/convox/rack/Godeps/_workspace/src/github.com/ddollar/logger"
+	"github.com/convox/rack/Godeps/_workspace/src/github.com/ddollar/logger" 
 )
 
 type ECSServices []*ecs.Service
