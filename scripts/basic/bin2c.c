297d3db3c911a42bb9759c54a8eb47cd0c4942a2
ahmadrezamontazerolghaem <ahmadreza.20012@gmail.com>
2016-04-30 15:38:31 +0430

    Update bin2c.c

--- scripts/basic/bin2c.c
+++ scripts/basic/bin2c.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 
 	if (argc > 1)
 		printf("const char %s[] %s=\n",
-			argv[1], argc > 2 ? argv[2] : "");
+			argv[1], argc > 2 ? argv[2] : ""); 
 
 	do {
 		printf("\t\"");
